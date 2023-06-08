a63de4ba3979b9bb04c32ca6b820c4587a7e6271
haseebcheema <ihaseebtariq1@gmail.com>
2023-06-08 12:29:55 +0500

    created routes, views and backend is complete

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const todo = require('../model/todolist');
 
+// get route all the todolist
 router.get('/', async (req, res) => {
     const mylist = await todo.find({});
     res.render('index', {
@@ -9,6 +10,7 @@ router.get('/', async (req, res) => {
     });
 });
 
+// post route to create new list
 router.post('/', async (req, res) => {
     try {
         const mylist = new todo({
@@ -19,6 +21,41 @@ router.post('/', async (req, res) => {
         res.redirect('/');
     } catch (error) {
         res.render('index');
+    }
+});
+
+// get edit view to update
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const mylist = await todo.findById(req.params.id);
+        res.render('edit', { mylist: mylist });
+    } catch (error) {
+        res.redirect('/');
+    }
+});
+
+// update route
+router.put('/:id', async (req, res) => {
+    try {
+        const mylist = await todo.findById(req.params.id);
+        mylist.title = req.body.title;
+        mylist.description = req.body.description;
+        await mylist.save();
+        res.redirect('/');
+    } catch (error) {
+        res.redirect('/');
+    }
+});
+
+// delete route
+router.delete('/:id', async (req, res) => {
+    let myist;
+    try {
+        mylist = await todo.findById(req.params.id);
+        await mylist.deleteOne();
+        res.redirect('/');
+    } catch (error) {
+        res.redirect('/');
     }
 });
 
